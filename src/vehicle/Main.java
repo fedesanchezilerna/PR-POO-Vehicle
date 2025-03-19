b61995b8b16d6dc9c88e5c50b142fe03ae0e60d0
Federico Sánchez <fedesanchezvidarte@gmail.com>
2025-03-19 11:31:53 +0100

    Optimizing imports

--- src/vehicle/Main.java
+++ src/vehicle/Main.java
@@ -1,9 +1,7 @@
 package vehicle;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
